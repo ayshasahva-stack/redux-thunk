d8c9cbfdcf71a5810128220b470c9bdbfae3ad1e
aysha sahva <ayshasahva@gmail.com>
2026-06-25 13:06:05 +0530

    second commit

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {getTaskFromServer} from '../slice/taskListSlice'
 
 const Tasks = () => {
+  const { taskLists, is_loading, error } = useSelector((state) => state.tasks)
+  // console.log(taskLists);
+  // console.log(is_loading);
+  // console.log(error);
+
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch(getTaskFromServer())
+
+  },[])
+
   return (
     <div>
       <h1>List Task</h1>
+
+      <div>
+        {taskLists.map((item)=>(
+          <div >
+            <p>{item.id}</p>
+            <p>{item.title}</p>
+            <p>{item.description}</p>
+            <hr />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- src/slice/taskListSlice.js
+++ src/slice/taskListSlice.js
@@ -1,7 +1,51 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    taskLists=[],
+    taskLists: [],
     is_loading: false,
     error: ""
 }
+
+export const getTaskFromServer = createAsyncThunk(
+    "tasks/getTaskFromServer",
+    async (_, { rejectWithValue }) => {
+        const response = await fetch("http://localhost:3001/tasks")
+        if (response.ok) {
+            const jsonResponse = await response.json()
+            return jsonResponse
+        } else {
+            return rejectWithValue("no task found")
+            // return rejectWithValue({error:"no task found"})
+        }
+
+    }
+
+)
+const taskListSlice = createSlice({
+    name: 'tasks',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            // get
+            .addCase(getTaskFromServer.pending, (state) => {
+                state.is_loading = true
+                state.error = ""
+            })
+            .addCase(getTaskFromServer.fulfilled, (state, action) => {
+                state.is_loading = false
+                state.taskLists = action.payload
+
+            })
+            .addCase(getTaskFromServer.rejected, (state) => {
+                state.is_loading = false
+                state.error = action.payload
+                state.taskLists = []
+
+            })
+
+    }
+
+})
+
+export default taskListSlice.reducer
